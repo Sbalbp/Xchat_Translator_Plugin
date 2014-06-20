78d7b7cabf03474a29069f2d3dac0049c745361d
Sbalbp <sbalbp@gmail.com>
2014-06-20 17:10:12 +0200

    Added translation functions
    Added a new callback to translate incoming messages in the channel.

--- src/translator_plugin.py
+++ src/translator_plugin.py
@@ -27,12 +27,49 @@ __module_author__ = "Sergio Balbuena <sbalbp@gmail.com>"
 import xchat
 import apertiumpluginutils.apertiumInterfaceAPY as iface
 import apertiumpluginutils.apertiumFiles as files
+try:
+    import html.parser as HTMLParser
+except:
+	import HTMLParser
 
+parser = HTMLParser.HTMLParser()
 errors_on = True
+
+def getFullChannel():
+	fullChannel = ''
+	list = xchat.get_list('channels')
+
+	if(list):
+		for i in list:
+			fullChannel = fullChannel+i.channel+'.'
+
+	return fullChannel
 
 def notify(text, info = True):
 	if(info or errors_on):
 		xchat.command('GUI MSGBOX \"'+text+'\"')
+
+def translate(text, user, direction):
+	result = None
+
+	if(direction != 'incoming' and direction != 'outgoing'):
+		return None
+
+	dictionary = files.getDictionary()[direction]
+
+	for key in [user+'@'+getFullChannel(), getFullChannel(), 'default']:
+		if(key in dictionary.keys()):
+			result = iface.translate(text, dictionary[key]['source'], dictionary[key]['target'])
+			break
+
+	if(result != None):
+		if(result['ok']):
+			result = result['result']
+		else:
+			result = None
+			notify(result['errorMsg'])
+
+	return result
 
 def apertium_apy_cb(word, word_eol, userdata):
 	if(len(word) <= 1):
@@ -68,6 +105,12 @@ def apertium_pairs_cb(word, word_eol, userdata):
 
 	return xchat.EAT_NONE
 
+def translate_cb(word, word_eol, userdata):
+	translation = translate(word[1],word[0],'incoming')
+
+	if(translation != None):
+		print('\ntranslation:\n'+(parser.unescape(translation)).encode('utf-8')+'\n')
+
 def unload_cb(userdata):
     files.save()
 
@@ -75,6 +118,9 @@ files.setFile('apertium_xchat_plugin_preferences.pkl')
 files.read()
 iface.setAPYAddress(files.getKey('apyAddress'))
 
+
 xchat.hook_unload(unload_cb)
 xchat.hook_command('apertium_apy', apertium_apy_cb, help='/apertium_apy <address>\nChanges the apy address where translation requests are sent. If no arguments are passed, it just shows the address.')
 xchat.hook_command('apertium_pairs', apertium_pairs_cb, help='/apertium_pairs\nShows all the available Apertium language pairs that can be used.')
+
+xchat.hook_print("Channel Message", translate_cb)
